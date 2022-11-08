c144a0a862eb043491bbdf3b6d9f7c2adcd51c1f
aitegin <zhanybaev1211@gmail.com>
2022-11-08 06:18:28 +0600

    Cursor effect complete

---
+++ index.js
@@ -0,0 +1,14 @@
+document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty (
+      '--x', (
+        e.clientX+window.scrollX
+      )
+      + 'px'
+    );
+    document.documentElement.style.setProperty (
+      '--y', (
+        e.clientY+window.scrollY
+      ) 
+      + 'px'
+    );
+  }
