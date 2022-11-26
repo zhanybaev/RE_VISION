38c1de285e671f26566df3517f644a2229327459
aitegin <zhanybaev1211@gmail.com>
2022-11-26 07:54:13 +0600

    Third block completed

--- index.js
+++ index.js
@@ -11,4 +11,15 @@ document.body.onmousemove = function(e) {
       ) 
       + 'px'
     );
-  }
+}
+
+const text1 = document.getElementsByClassName('block3__text-first')[0];
+const text2 = document.getElementsByClassName('block3__text-second')[0];
+document.addEventListener('scroll', ()=>{
+  console.log(window.scrollY);
+  const moveRight = -22.8297872 + (0.0191489362 * window.scrollY)
+  console.log(moveRight);
+  const moveLeft = 54.5170843 + (-0.020501139 * window.scrollY)
+  text1.setAttribute('style', `transform: translateX(${moveRight}%)`)
+  text2.setAttribute('style', `transform: translateX(${moveLeft}%)`)
+})
