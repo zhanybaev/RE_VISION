79e75867451decc933829861f516f143ca7b557a
aitegin <zhanybaev1211@gmail.com>
2022-11-26 12:16:51 +0600

    Cursor bag on scroll fixed

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let marginTop=0;
 document.body.onmousemove = function(e) {
     document.documentElement.style.setProperty (
       '--x', (
@@ -11,15 +12,27 @@ document.body.onmousemove = function(e) {
       ) 
       + 'px'
     );
+    marginTop = e.clientY
 }
-
+window.scroll({
+  top: 100, 
+  left: 0, 
+  behavior: 'smooth'
+});
 const text1 = document.getElementsByClassName('block3__text-first')[0];
 const text2 = document.getElementsByClassName('block3__text-second')[0];
 document.addEventListener('scroll', ()=>{
-  console.log(window.scrollY);
   const moveRight = -22.8297872 + (0.0191489362 * window.scrollY)
-  console.log(moveRight);
   const moveLeft = 54.5170843 + (-0.020501139 * window.scrollY)
   text1.setAttribute('style', `transform: translateX(${moveRight}%)`)
   text2.setAttribute('style', `transform: translateX(${moveLeft}%)`)
+  
+  document.documentElement.style.setProperty (
+    '--y', (
+      window.scrollY + marginTop
+    ) 
+    + 'px'
+  )
+  
+  console.log(marginTop);
 })
